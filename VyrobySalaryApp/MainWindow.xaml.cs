4689d4a029cdb3bda521021d47bebc989c391c31
Sergdenis73 <sergdenis1973@gmail.com>
2026-05-06 18:33:07 +0300

    Add test data to DataGrid
    Додано клас WorkerProduct і тимчасове заповнення таблиці тестовими даними.

--- VyrobySalaryApp/MainWindow.xaml.cs
+++ VyrobySalaryApp/MainWindow.xaml.cs
@@ -1,24 +1,59 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VyrobySalaryApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<WorkerProduct> workersList;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            workersList = new ObservableCollection<WorkerProduct>();
+
+            workersList.Add(new WorkerProduct
+            {
+                id = 1,
+                surname = "Денисенко",
+                workshop_number = 1,
+                product_a = 12,
+                product_b = 8,
+                product_c = 5
+            });
+
+            workersList.Add(new WorkerProduct
+            {
+                id = 2,
+                surname = "Шкурат",
+                workshop_number = 1,
+                product_a = 10,
+                product_b = 6,
+                product_c = 7
+            });
+
+            workersList.Add(new WorkerProduct
+            {
+                id = 3,
+                surname = "Іваненко",
+                workshop_number = 2,
+                product_a = 15,
+                product_b = 9,
+                product_c = 4
+            });
+
+            workersList.Add(new WorkerProduct
+            {
+                id = 4,
+                surname = "Петренко",
+                workshop_number = 2,
+                product_a = 7,
+                product_b = 11,
+                product_c = 6
+            });
+
+            ProductsDataGrid.ItemsSource = workersList;
         }
     }
 }
---
+++ VyrobySalaryApp/WorkerProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VyrobySalaryApp
+{
+    public class WorkerProduct
+    {
+        public int id { get; set; }
+
+        public string surname { get; set; } = "";
+
+        public int workshop_number { get; set; }
+
+        public int product_a { get; set; }
+
+        public int product_b { get; set; }
+
+        public int product_c { get; set; }
+    }
+}
