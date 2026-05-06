90f446af942ed966b63820a9dea6d4f79cc08a10
Sergdenis73 <sergdenis1973@gmail.com>
2026-05-07 00:24:15 +0300

    Connect application to MySQL
    Додано клас DataAccess і завантаження даних із таблиці workers_products у DataGrid

---
+++ VyrobySalaryApp/DataAccess.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace VyrobySalaryApp
+{
+    public class DataAccess
+    {
+        private string connStr = "Server=localhost;Database=vyroby_salary_db;Uid=root;Pwd=Sergdenis@73;";
+
+        public ObservableCollection<WorkerProduct> LoadWorkersProducts()
+        {
+            ObservableCollection<WorkerProduct> list = new ObservableCollection<WorkerProduct>();
+
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(connStr);
+                conn.Open();
+
+                string sql = "SELECT id, surname, workshop_number, product_a, product_b, product_c FROM workers_products;";
+
+                MySqlCommand command = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    WorkerProduct worker = new WorkerProduct();
+
+                    worker.id = reader.GetInt32("id");
+                    worker.surname = reader.GetString("surname");
+                    worker.workshop_number = reader.GetInt32("workshop_number");
+                    worker.product_a = reader.GetInt32("product_a");
+                    worker.product_b = reader.GetInt32("product_b");
+                    worker.product_c = reader.GetInt32("product_c");
+
+                    list.Add(worker);
+                }
+
+                reader.Close();
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Не вдалося підключитися до бази даних MySQL.");
+            }
+
+            return list;
+        }
+    }
+}
--- VyrobySalaryApp/MainWindow.xaml.cs
+++ VyrobySalaryApp/MainWindow.xaml.cs
@@ -11,52 +11,20 @@ namespace VyrobySalaryApp
         {
             InitializeComponent();
 
-            workersList = new ObservableCollection<WorkerProduct>();
-
-            workersList.Add(new WorkerProduct
-            {
-                id = 1,
-                surname = "Денисенко",
-                workshop_number = 1,
-                product_a = 12,
-                product_b = 8,
-                product_c = 5
-            });
-
-            workersList.Add(new WorkerProduct
-            {
-                id = 2,
-                surname = "Шкурат",
-                workshop_number = 1,
-                product_a = 10,
-                product_b = 6,
-                product_c = 7
-            });
-
-            workersList.Add(new WorkerProduct
-            {
-                id = 3,
-                surname = "Іваненко",
-                workshop_number = 2,
-                product_a = 15,
-                product_b = 9,
-                product_c = 4
-            });
-
-            workersList.Add(new WorkerProduct
-            {
-                id = 4,
-                surname = "Петренко",
-                workshop_number = 2,
-                product_a = 7,
-                product_b = 11,
-                product_c = 6
-            });
-
-            ProductsDataGrid.ItemsSource = workersList;
+            LoadDataFromDb();
 
             ApplyAccessRights();
         }
+
+        private void LoadDataFromDb()
+        {
+            DataAccess dataAccess = new DataAccess();
+
+            workersList = dataAccess.LoadWorkersProducts();
+
+            ProductsDataGrid.ItemsSource = workersList;
+        }
+
         private void ApplyAccessRights()
         {
             if (Authorization.CurrentRole == "Бухгалтер")
@@ -72,6 +40,7 @@ namespace VyrobySalaryApp
                 StatusTextBlock.Text = "Користувач: Керівник. Режим перегляду.";
             }
         }
+
         private void AuthMenuItem_Click(object sender, RoutedEventArgs e)
         {
             LogInForm loginForm = new LogInForm();
